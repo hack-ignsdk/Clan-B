d8c9981df2d80d630e4df6df744f8f72d45037a1
Rizqi Bintang <rizqibintang95@in.com>
2014-04-20 12:11:24 +0700

    Memasukkan source code PHP

---
+++ PHP/index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>.:: Agipz Larcenciels Library ::.</title>
+<style type="text/css">
+body {
+background-color: #f2f2f2;
+color: black;
+margin-top: 0px;
+margin-left: 50px;
+margin-right: 50px;
+font-family: arial;
+font-size: 14px;
+}
+.style1 {color: #FFFFFF}
+.style2 {color: #000000}
+</style>
+</head>
+
+<body>
+<center>
+<table border=0 width="900px" bgcolor="#000000" colspan="0">
+	<tr>
+	<form action="login.php" name="login" method="post">
+		<td colspan="2" align="right" bgcolor="#000000" style="background: #B8D197; padding: 8px 0 8px 0">
+		Username : 
+		  <input type="text" name="user" placeholder="perpus" required> Password : <input type="password" name="pass" placeholder="bismillah" required> <input type="submit" value="log in">
+		</td>
+	</form>
+	</tr>
+	<tr>
+		<td colspan="2" width="900px" height="200px"><img src="image/logo2.png" width="100%" height="180px"></td>
+	</tr>
+	<tr>
+		<td colspan="2" bgcolor="#000000">
+		  <span class="style1">
+		  <span class="style2"> <span class="style1">
+		  <marquee behavior="alternate" direction="left" onMouseOver="this.stop();" onMouseOut="this.start();">
+		  <strong>Welcome in Agipz Larcenciels Library </strong>
+	      </marquee>
+	    </span> </span> </span></td>
+	
+	</tr>
+	<tr>
+	<td width="40%" valign="top" align="center" >&nbsp;</td>
+	<td width="60%" style="background: #A7BCCD">&nbsp;</td>
+	<tr>
+	<td colspan="2" style="background: #88A695; border-top: solid 2px #f9f9f9; font-size: 12px" align="center">&nbsp;</td>
+	</tr>
+</table>
+</center>
